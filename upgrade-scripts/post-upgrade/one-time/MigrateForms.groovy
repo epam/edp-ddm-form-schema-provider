44bfdd77cdf2d7e8bee71ddd40260c9f2d762417
Oleksandr Lazebnyi <oleksandr_lazebnyi@epam.com>
2022-11-11 15:04:55 +0000

    [EDP-DDM-00000] Added deploymentMode
    Change-Id: I0ea9b7135d843eeb002d8c0721328d14b245872d

--- upgrade-scripts/post-upgrade/one-time/MigrateForms.groovy
+++ upgrade-scripts/post-upgrade/one-time/MigrateForms.groovy
@@ -8,8 +8,10 @@ void call() {
 
     String stages = '%5B%7B%22stages%22%3A%20%5B%7B%22name%22%3A%20%22checkout%22%7D%2C%7B%22name%22%3A%20%22init-registry%22%7D%2C%7B%22name%22%3A%20%22get-changes%22%7D%2C%7B%22name%22%3A%20%22registry-regulations-validation%22%7D%2C%7B%22name%22%3A%20%22shutdown-services%22%7D%2C%7B%22name%22%3A%20%22create-backup%22%7D%2C%7B%22name%22%3A%20%22upload-form-changes%22%7D%5D%7D%5D'
 
+    String DEPLOYMENT_MODE = sh(script: "helm get values registry-configuration -n $NAMESPACE | grep 'deploymentMode: ' | awk '{print \$2}'", returnStdout: true).trim()
+
     sh "curl -XPOST \"${JENKINS_URL_WITH_CREDS}/job/registry-regulations/job/MASTER-Build-registry-regulations/buildWithParameters?" +
-            "FULL_DEPLOY=true&STAGES=${stages}\""
+            "FULL_DEPLOY=true&STAGES=${stages}&DEPLOYMENT_MODE=$DEPLOYMENT_MODE\""
 }
 
 return this;
